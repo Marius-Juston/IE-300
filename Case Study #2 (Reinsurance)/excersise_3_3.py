Fixed estimator calculation mistake

Case Study #2 (Reinsurance)/excersise_3_3.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 
+
 def estimator_1(x, R):
-    diff = np.max(x - R, 0)
+    diff = (x - R)
+    diff[diff < 0] = 0
 
     return np.sum(diff) / x.shape[0]
 

Case Study #2 (Reinsurance)/excersise_3_5.py
@@ -5,7 +5,7 @@ import pandas as pd
 def hill_estimator(x, k, x_k_n):
     n = x.shape[0]
 
-    return 1 / k * np.sum(np.log(x[n - k:])) - np.log(x_k_n)
+    return 1 / k * np.sum(np.log(x[n - k:]) - np.log(x_k_n))
 
 
 def estimator_3(x, k, R):